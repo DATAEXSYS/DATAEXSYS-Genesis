41ca1780caafcc77e99815519e044100bf6b390a
Salman <mmsa155j3@gmail.com>
2025-12-09 10:06:56 +0500

    Basic Setup for Simulator

--- inc/Packet/basicPacket.h
+++ inc/Packet/basicPacket.h
@@ -1,7 +1,15 @@
 #pragma once
 #include <cstdint>
+#include <vector>
+class BasicPacket {
+public:
+    uint16_t source_id; //2 bytes
+    uint16_t destination_id; //2 bytes
+    uint32_t sequence_number; // 4 bytes
+    uint64_t timestamp; // 8 bytes
+    std::vector<uint8_t> payload;//1474 bytes
 
-struct BasicPacket {
-    uint8_t type;
-    uint16_t length;
+    BasicPacket(uint16_t src, uint16_t dest, uint32_t seq_num, uint64_t time, const std::vector<uint8_t>& data)
+        : source_id(src), destination_id(dest), sequence_number(seq_num), timestamp(time), payload(data) {}
 };
+
