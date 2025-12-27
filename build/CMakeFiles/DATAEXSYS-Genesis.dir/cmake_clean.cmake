f94a99cf2071a472c3d56200f61351edb39d1a88
Muhammad Salman Amir <mmsa155j3@gmail.com>
2025-12-27 20:49:32 +0500

    ProjectSetup

---
+++ build/CMakeFiles/DATAEXSYS-Genesis.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DATAEXSYS-Genesis.dir/test.cpp.o"
+  "CMakeFiles/DATAEXSYS-Genesis.dir/test.cpp.o.d"
+  "DATAEXSYS-Genesis"
+  "DATAEXSYS-Genesis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DATAEXSYS-Genesis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/DATAEXSYS-Genesis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DATAEXSYS-Genesis.
+# This may be replaced when dependencies are built.
